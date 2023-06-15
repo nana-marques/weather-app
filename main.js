65117fa19f0068796a7fdad4092bab667d43f5d4
nana-marques <annamarques16@hotmail.com>
2023-06-15 17:03:28 -0300

    testando alternativas

--- main.js
+++ main.js
@@ -1,42 +1,25 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import { api } from "./keys.js";
+// const {apikeys} = require("./keys.js");
+// console.log(apikeys);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+console.log(api2);
 
-setupCounter(document.querySelector('#counter'))
+const apikey = '72f0cd752e8965e979958e5388c99f3f';
 
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
 
-// const apikey = '72f0cd752e8965e979958e5388c99f3f';
-// const apiurl = "https://api.openweathermap.org/data/2.5/weather?lat=51.5&lon=10.5&units=metric";
+//console.log(import.meta.env.VITE_API_KEY);
 
-// const search = document.querySelector(".search input");
-// const searchBtn = document.querySelector(".search button");
+const search = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-// async function checkWeather(city){
-//     const response = await fetch(apiurl + city +`&appid=${apikey}`);
-//     var data = await response.json();
-//     console.log(data);
+async function checkWeather(city){
+    const response = await fetch(apiurl + city +`&appid=${apikey}`);
+    var data = await response.json();
+    console.log(data);
 
-//     document.querySelector(".city").innerHTML = data.name;
-//     document.querySelector(".number").innerHTML = Math.round(data.main.temp);
-// }
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".number").innerHTML = Math.round(data.main.temp);
+}
 
-// checkWeather();
+checkWeather("lat=51.5&lon=10.5");
