3723a0bdb29183427ce45820fbf7bddd9ef9e577
nana-marques <annamarques16@hotmail.com>
2023-06-16 16:41:46 -0300

    inicializando search

--- main.js
+++ main.js
@@ -1,20 +1,60 @@
 import { api } from './keys.js';
 console.log(api.key);
 
-const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
-
+const url1 = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+const url2 = "http://api.openweathermap.org/geo/1.0/direct?q=";
 //console.log(import.meta.env.VITE_API_KEY);
 
-const search = document.querySelector(".search input");
+const search = document.getElementById("input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".image");
+
+
 
 async function checkWeather(city){
-    const response = await fetch(apiurl + city +`&appid=${api.key}`);
-    var data = await response.json();
-    console.log(data);
+    // const responses = await fetch(apiurl + lat +`&appid=${api.key}`);
+    //const convert = await fetch(url2 + input +`&appid=${api.key}`);
+    console.log(city.value);
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".number").innerHTML = Math.round(data.main.temp);
+    const response1 = await fetch(url2 + city +`&appid=${api.key}`);
+    console.log(response1)
+
+    var data1 = await response1.json();
+    console.log(data1);
+
+    const coordinates = data1.find((data1) => data1.name === city)
+    console.log(coordinates)
+
+    // const response2 = await fetch(url1 + convert +`&appid=${api.key}`);
+    // var data2 = await response2.json();
+    // console.log(data2);
+
+    // document.querySelector(".city").innerHTML = data2.name;
+    // document.querySelector(".number").innerHTML = Math.round(data2.main.temp);
+
+    // switch(data2.weather[0].main){
+    //     case "Clouds":
+    //         weatherIcon.src = "images/clouds.png"
+    //         break;
+    //     case "Clear":
+    //         weatherIcon.src = "images/clear.png"
+    //         break;
+    //     case "Rain":
+    //         weatherIcon.src = "images/rain.png"
+    //         break;
+    //     case "Drizzle":
+    //         weatherIcon.src = "images/drizzle.png"
+    //         break;
+    //     case "Mist":
+    //         weatherIcon.src = "images/clouds.png"
+    //         break;
+    //     case "Snow":
+    //         weatherIcon.src = "images/snow.png"
+    //         break;
+    //     default:
+    //         weatherIcon.src = "images/clouds.png"
+    //         break;
+    // }
 }
 
-checkWeather("lat=51.5&lon=10.5");
+checkWeather(search);
